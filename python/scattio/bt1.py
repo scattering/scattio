3c113ceda8fd42b0390caa84bc8b044fce24cab2
Paul Kienzle <paul.kienzle@nist.gov>
2013-04-11 19:06:46 -0400

    support bt1 files back to 1997

--- python/scattio/bt1.py
+++ python/scattio/bt1.py
@@ -3,12 +3,18 @@ import time
 
 import numpy
 
-#==== Example file =====
+#==== fpx file ====
+# Motor no.  4    Intensity at Detector:    1Dec 10 2010 16:17
+#    0.9990          5684
+#    ...
+
+
+#==== ICP file =====
 #'srnaf002.bt1' 'Dec 18 2012' 1   0   0   0 1 1  142284.    1  'NEUT'  201  'RAW'
 #  Filename         Date       Exp. Parameters     Mon     Prf  Base   #pts  Type
-#srnaf Sr1.25Na1.5Fe5O2(PO4)5 3.5g Ge311/60/ 150K  
+#srnaf Sr1.25Na1.5Fe5O2(PO4)5 3.5g Ge311/60/ 150K
 #  60   20    7    0   0   0   0   2.0780     0.00000 0.00000   0.00000   32
-# Collimation      Mosaic    Wavelength   T-Start   Incr.   H-field #Det    
+# Collimation      Mosaic    Wavelength   T-Start   Incr.   H-field #Det
 #  3      0.0000   0.0000   0.0000
 #  4      1.3000   0.0500  11.3000
 # Mot:    Start       Step      End
@@ -36,7 +42,16 @@ import numpy
 
 
 class ICPBT1(object):
+    MONOCHROMATORS = dict([
+        ('Ge(311)', 2.079),
+        ('Cu(311)', 1.540),
+        ('Ge(733)', 1.197),
+        ])
     def __init__(self, filename):
+        basename = os.path.basename(filename)
+        if os.path.getsize(filename) == 0:
+            raise ValueError('empty file')
+
         with open(filename, 'rt') as file:
             self.path = filename
             self._read_file(file)
@@ -46,13 +61,18 @@ class ICPBT1(object):
         self._read_motors(file)
         self._read_blades(file)
         self._read_data(file)
-    
+
     def _read_parameters(self, file):
-        lines = [file.readline() for _ in range(5)]
-        #print "lines[0]",lines[0],lines[0].split("'")
-        _, filename, _, date, bits, base, pts, type, _ = lines[0].split("'") 
+        line0 = file.readline()
+        if line0.startswith(' Motor'): raise IOError("findpeak files not supported")
+        if "Start of Run" in line0: raise IOError("monrec files not supported")
+
+        _, filename, _, date, bits, base, pts, type, _ = line0.split("'")
         self.filename = filename
-        self.date = time.strptime(date, '%b %d %Y')
+        try:
+            self.date = time.strptime(date, '%b %d %Y')
+        except ValueError:
+            self.date = time.strptime(date, '%d-%b-%Y')
         bits = bits.split()
         self.experiment_parameters = [int(s) for s in bits[:6]]
         self.monitor = float(bits[6])
@@ -60,15 +80,31 @@ class ICPBT1(object):
         self.base = base
         self.pts = int(pts)
         self.type = type
-        self.comment = lines[2].strip()
-        bits = lines[3].split()
+
+        _,line2,line3,_ = [file.readline() for _ in range(4)]
+
+        self.comment = line2.strip()
+        bits = line3.split()
         self.collimation = [int(s) for s in bits[:3]]
         self.mosaic = [int(s) for s in bits[3:7]]
-        self.wavelength = float(bits[7])
+        self.recorded_wavelength = float(bits[7])
+        for k,v in self.MONOCHROMATORS.items():
+            if abs(self.recorded_wavelength - v) < 0.1:
+                self.wavelength = v
+                self.monochromator = k
+                break
+        else:
+            self.wavelength = self.recorded_wavelength
+            self.monochromator = 'unknown'
+
         self.Tstart = float(bits[8])
         self.Tincr = float(bits[9])
         self.Hfield = float(bits[10])
-        self.numdetectors = int(bits[11])
+        # Jan '04 files were missing number of detectors
+        try:
+            self.numdetectors = int(bits[11])
+        except IndexError:
+            self.numdetectors = 32
 
     def _read_blades(self, file):
         lines = [file.readline() for _ in range(8)]
@@ -84,35 +120,43 @@ class ICPBT1(object):
         columns = []
         monitors = []
         line = file.readline()
+
+        if line.startswith('$Rocking'):
+            line = file.readline()
+
+
         # Get column names from
         #     $ M4=     11.000  T= 250.770 C=   0.81 N=           0
         self.column_names = [s.split()[-1] for s in line.split('=')[:-1]]
         self.column_number = dict((c,i) for i,c in enumerate(self.column_names))
+        # $Rocking motor  3 between  -10.00 and   10.0
         # Final data block is "$ timestamp"
         while '=' in line:
             fields = [s.split()[0] for s in line.split('=')[1:]]
             columns.append([float(s) for s in fields])
             line = file.readline()
-            monitors.append([int(s) for s in line[1:].split()])
-            line = file.readline()
+            if line.startswith('$'):
+                monitors.append([int(s) for s in line[1:].split()])
+                line = file.readline()
             parts = []
             while line.startswith(' '):
                 parts.append(line)
                 line = file.readline()
             joined_line = "".join(s.strip() for s in parts)
+            if joined_line.endswith(','): joined_line = joined_line[:-1]
             detectors.append([int(s) for s in joined_line.split(',')])
-            
-            while line.startswith('$ Stat Fail:') or line.startswith('$ Main_Stat Fail:'):
+
+            while 'ail:' in line:
                 # silently skip?
                 line = file.readline()
-            
-        try:
-            self.timestamp = time.strptime(line[2:22], '%d-%b-%Y %H:%M:%S')
-        except:
-            self.timestamp = self.date
-            line = line.strip()
-            if line != '':
-                print "could not parse timestamp %r in %r"%(line.strip(),self.path)
+        self.timestamp = self.date # Default timestamp to date at start of file
+        if line.startswith('$'):
+            try:
+                self.timestamp = time.strptime(line[2:22], '%d-%b-%Y %H:%M:%S')
+            except:
+                line = line.strip()
+                if line != '':
+                    print >>sys.stderr,"could not parse timestamp %r in %r"%(line.strip(),self.path)
         self.counts = numpy.array(detectors)
         self.monitors = numpy.array(monitors)
         self.columns = numpy.array(columns)
@@ -124,7 +168,12 @@ class ICPBT1(object):
             words=line.split()
             if words[0] == 'Mot:': break
             name = words[0] if not words[0].isdigit() else 'M'+words[0]
-            start,step,stop = [float(s) for s in words[1:]]
+            try:
+                start,step,stop = [float(s) for s in words[1:]]
+            except:
+                # fortran uses fixed width fields, which may run into each other
+                # if the simple parse doesn't work, try the known field widths.
+                start,step,stop = float(line[6:15]),float(line[15:24]),float(line[24:33])
             motors.append((name,start,step,stop))
         self.motors = motors
 
@@ -132,64 +181,82 @@ def monavg(plot=False):
     import sys
     import pylab
     import datetime
-    
+
     files = sys.argv[1:]
     summary = {}
+    count = 0
     for f in files:
         try:
             data = ICPBT1(f)
-        except:
-            if not os.path.basename(f).startswith('fpx'): print "skipping",f
+        except Exception,exc:
+            #import traceback
+            #traceback.print_exc()
+            if 'files not supported' in exc.message or 'empty' in exc.message:
+                pass
+            else:
+                raise
+                print >>sys.stderr,"error",exc.message,f
             continue
         if data.base != 'NEUT':
-            print >>sys.stderr, "by time",f
+            #print >>sys.stderr, "by time",f
+            pass
         elif 'C' not in data.column_number:
+            #print data.columns_names
             print >>sys.stderr, "missing count time",f
         else:
             times = data.columns[:,data.column_number['C']]
             u,s = numpy.mean(times),numpy.std(times)
             #print data.path,times
             if s/u > 0.02 and (u > 1 or s > 0.1):
-                print >>sys.stderr, "high count time variation (%10.3f +/- %10.3f) in %r"%(u,s,data.path)
+                #print >>sys.stderr, "high count time variation %d%% (%10.3f +/- %10.3f) in %r"%(int(100*s/u),u,s,data.path)
+                pass
             monitors = data.monitor*data.prefactor*len(times)
             seconds = sum(times)*60
-            collimation = "-".join("%d"%c for c in data.collimation)
-            points = summary.setdefault((collimation,data.wavelength),[])
+            key = "%s %2d'"%(data.monochromator,data.collimation[0])
+            points = summary.setdefault(key,[])
             points.append((data.timestamp,monitors/seconds,data.path))
-    
+            count += 1
+
+    if len(summary) > 7:
+        items = list(sorted((len(v),k) for k,v in summary.items()))
+        _,saved_keys = zip(*items[-7:])
+        summary = dict([(k,summary[k]) for k in saved_keys])
+
+    plot = plot and not count < 5
     for k,v in sorted(summary.items()):
         if plot:
-            if len(v) < 10: continue
-            coll,wavelen = k
+            #if len(v) < 10: continue
             dates,rates,paths = zip(*v)
             dates = [datetime.datetime.fromtimestamp(time.mktime(s)) for s in dates]
-            pylab.plot(pylab.date2num(dates),rates, 'o',
-                       label=coll+" %.3f"%wavelen,
-                       hold=True)
+            pylab.plot(pylab.date2num(dates),rates, 'o', label=k, hold=True)
         else:
-            print "== %s %.3f =="%k
+            print "== %s =="%k
             for date,rate,path in v:
                 stamp = time.strftime("%Y-%m-%d %H:%M:%S",date)
                 print stamp,"%10.3f"%rate,path
-                
+
     if plot:
         pylab.grid(True)
         ax = pylab.gca()
-        ax.xaxis_date()
         ax.xaxis.set_major_locator(pylab.MonthLocator(interval=3))
-        ax.xaxis.set_minor_locator(pylab.WeekdayLocator(pylab.MO))
+        ax.xaxis.set_minor_locator(pylab.MonthLocator(interval=1,bymonthday=(1,15)))
         ax.xaxis.set_major_formatter(pylab.DateFormatter("%b '%y"))
-        pylab.legend()
+        #ax.xaxis_date()
+        pylab.title('BT-1 monitor rate for various monochromator-collimator configurations')
+        pylab.xlabel('Time')
+        pylab.ylabel('Monitor Rate (counts/second)')
+        legend = pylab.legend(loc='best', fancybox=True, numpoints=1)
+        legend.get_frame().set_alpha(0.7)
         pylab.show()
 
 def demo():
     import sys
     from pprint import pprint
-    
+
     for f in sys.argv[1:]:
         data = ICPBT1(f)
         pprint(data.__dict__)
-        
+
 if __name__ == "__main__":
     #demo()
     monavg(plot=True)
